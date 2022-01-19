3cf89c1ad501929171ecd3646d8430ae6f2a4d07
DheerajKumar <dheerajkodam@gmail.com>
2022-01-19 16:15:32 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Link,Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage=() =>(
     <div>
-        <h1>HATS PAGE</h1>
+        <Link to="/">HATSPAGE</Link>
     </div>
 );
 
+const Shop=() =>(
+    <div>
+        <Link to="/hats">HATSPAGE</Link>
+    </div>
+);
+    
+
 function App() {
     return (
-        <div>
-            <Route exact path='/' component={HomePage} />
-            <Route  path='/hats' component={HatsPage} />
-
-        </div>
+            <Routes>
+            <Route exact path='/' element={<HomePage/>} />
+            <Route  path='/hats' element={<HatsPage/>} />
+            <Route path='/shop' element={<Shop/>}/>
+            <Route/>
+            </Routes>
     )
 }
+
 
 export default App;
