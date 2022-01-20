027cff5fc74c2b5622a1b78bae11165395d83126
DheerajKumar <dheerajkodam@gmail.com>
2022-01-20 10:46:41 +0530

    updating our app with shop page, collection-item, collection-preview

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import './App.css';
 import { Link,Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage=() =>(
-    <div>
-        <Link to="/">HATSPAGE</Link>
-    </div>
-);
 
-const Shop=() =>(
-    <div>
-        <Link to="/hats">HATSPAGE</Link>
-    </div>
-);
+// const HatsPage=() =>(
+//     <div>
+//         <Link to="/">HATSPAGE</Link>
+//     </div>
+// );
+
+// const Shop=() =>(
+//     <div>
+//         <Link to="/hats">HATSPAGE</Link>
+//     </div>
+// );
     
 
 function App() {
     return (
             <Routes>
             <Route exact path='/' element={<HomePage/>} />
-            <Route  path='/hats' element={<HatsPage/>} />
-            <Route path='/shop' element={<Shop/>}/>
+            <Route path='/shop' element={<ShopPage/>}/>
             <Route/>
             </Routes>
     )
